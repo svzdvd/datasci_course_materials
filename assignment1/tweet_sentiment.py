bcc3a0769584ccdc51cdfb322e8c5530cfdd9097
Davide Savazzi <davide@davidesavazzi.net>
2014-07-19 14:50:23 +0200

    completed tweet_sentiment assignment

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,31 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sent_file_name = sys.argv[1]
+    tweet_file_name = sys.argv[2]
+
+    # initialize word scores
+    scores = {}
+    sent_file = open(sent_file_name)
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
+    # analyze tweets
+    tweet_file = open(tweet_file_name)
+    for line in tweet_file:
+        tweet = json.loads(line)
+        if 'text' in tweet:
+            if 'lang' not in tweet or tweet['lang'] == 'en':
+                words = tweet['text'].split(" ")
+                score = 0
+                for word in words:
+                    if word in scores:
+                        score += scores[word]
+                print score
+
 
 if __name__ == '__main__':
     main()
